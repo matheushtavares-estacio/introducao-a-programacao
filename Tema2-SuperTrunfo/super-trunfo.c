2d14d5dacac9ff3c22d54cc327d27aa75475ce7c
matheushtavares-estacio <202511091108@alunos.estacio.br>
2026-01-31 00:59:06 +0000

    Finalizando desafio Super Trunfo - Nível Novato

---
+++ Tema2-SuperTrunfo/super-trunfo.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main(){
+    // === VARIÁVEIS ===
+    char estado1, estado2;
+    char codigo1[10], codigo2[10];
+    char cidade1[30], cidade2[30];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int num_pturisticos1, num_pturisticos2;
+
+    // === CADASTRO CARTA #1 ===
+    printf("\n=== CADASTRO CARTA #1 ===\n");
+    
+    printf("ESTADO: ");
+    scanf(" %c", &estado1);
+
+    printf("CÓDIGO: ");
+    scanf("%s", codigo1);
+
+    printf("CIDADE: ");
+    scanf("%s", cidade1);
+
+    printf("POPULAÇÃO: ");
+    scanf("%d", &populacao1);
+
+    printf("ÁREA: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("NÚMERO DE PONTOS TURÍSTICOS: ");
+    scanf("%d", &num_pturisticos1);
+
+    // === CADASTRO CARTA #2 ===
+    printf("\n=== CADASTRO CARTA #2 ===\n");
+    
+    printf("ESTADO: ");
+    scanf(" %c", &estado2);
+
+    printf("CÓDIGO: ");
+    scanf("%s", codigo2);
+
+    printf("CIDADE: ");
+    scanf("%s", cidade2);
+
+    printf("POPULAÇÃO: ");
+    scanf("%d", &populacao2);
+
+    printf("ÁREA: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("NÚMERO DE PONTOS TURÍSTICOS: ");
+    scanf("%d", &num_pturisticos2);
+
+    // === PRINT CARTA #1 ===
+    printf("\n=== CARTA #1 ===\n");
+    printf("ESTADO: %c\n", estado1);
+    printf("CÓDIGO: %s\n", codigo1);
+    printf("CIDADE: %s\n", cidade1);
+    printf("POPULAÇÃO: %d\n", populacao1);
+    printf("ÁREA: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos1);
+
+    // === PRINT CARTA #2 ===
+    printf("\n=== CARTA #2 ===\n");
+    printf("ESTADO: %c\n", estado2);
+    printf("CÓDIGO: %s\n", codigo2);
+    printf("CIDADE: %s\n", cidade2);
+    printf("POPULAÇÃO: %d\n", populacao2);
+    printf("ÁREA: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos2);
+
+    return 0;
+}
