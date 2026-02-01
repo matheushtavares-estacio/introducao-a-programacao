948ef14491f032ab4da0c5c4a1bdb0642ef8b051
matheushtavares-estacio <202511091108@alunos.estacio.br>
2026-02-01 15:15:25 +0000

    Tema 3: Atividade Nível Novato - Implementação de estruturas de decição para definir a carta ganhadora em 1 dos atributos.

---
+++ Tema3-SuperTrunfo/super-trunfo.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+
+int main(){
+    // === VARIÁVEIS ===
+    char estado1, estado2;
+    char codigo1[10], codigo2[10];
+    char cidade1[30], cidade2[30];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int num_pturisticos1, num_pturisticos2;
+    float den_populacional1, den_populacional2;
+    float pib_pcapita1, pib_pcapita2;
+    float super_poder1, super_poder2;
+    float inv_den_populacional1, inv_den_populacional2;
+
+    // === CADASTRO CARTA #1 ===
+    printf("\n=== CADASTRO CARTA #1 ===\n");
+    
+    printf("ESTADO: ");
+    scanf(" %c", &estado1);
+
+    printf("CÓDIGO: ");
+    scanf("%s", codigo1);
+
+    printf("CIDADE: ");
+    scanf("%s", cidade1);
+
+    printf("POPULAÇÃO: ");
+    scanf("%d", &populacao1);
+
+    printf("ÁREA: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("NÚMERO DE PONTOS TURÍSTICOS: ");
+    scanf("%d", &num_pturisticos1);
+
+    // === CADASTRO CARTA #2 ===
+    printf("\n=== CADASTRO CARTA #2 ===\n");
+    
+    printf("ESTADO: ");
+    scanf(" %c", &estado2);
+
+    printf("CÓDIGO: ");
+    scanf("%s", codigo2);
+
+    printf("CIDADE: ");
+    scanf("%s", cidade2);
+
+    printf("POPULAÇÃO: ");
+    scanf("%d", &populacao2);
+
+    printf("ÁREA: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("NÚMERO DE PONTOS TURÍSTICOS: ");
+    scanf("%d", &num_pturisticos2);
+
+    // === CALCULOS ===
+    den_populacional1 = (float)populacao1 / area1;
+    den_populacional2 = (float)populacao2 / area2;
+    pib_pcapita1 = pib1 / (float)populacao1;
+    pib_pcapita2 = pib2 / (float)populacao2;
+    inv_den_populacional1 = 1.0 / den_populacional1;
+    inv_den_populacional2 = 1.0 / den_populacional2;
+    super_poder1 = (float)populacao1 + area1 + pib1 + (float)num_pturisticos1 + pib_pcapita1 + inv_den_populacional1;
+    super_poder2 = (float)populacao2 + area2 + pib2 + (float)num_pturisticos2 + pib_pcapita2 + inv_den_populacional2;
+
+    // === PRINT CARTA #1 ===
+    printf("\n=== CARTA #1 ===\n");
+    printf("ESTADO: %c\n", estado1);
+    printf("CÓDIGO: %s\n", codigo1);
+    printf("CIDADE: %s\n", cidade1);
+    printf("POPULAÇÃO: %d\n", populacao1);
+    printf("ÁREA: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos1);
+    printf("DENSIDADE POPULACIONAL: %.2f\n", den_populacional1);
+    printf("PIB PER CAPITA: %.2f\n", pib_pcapita1);
+    printf("SUPER PODER: %.2f\n", super_poder1);
+
+    // === PRINT CARTA #2 ===
+    printf("\n=== CARTA #2 ===\n");
+    printf("ESTADO: %c\n", estado2);
+    printf("CÓDIGO: %s\n", codigo2);
+    printf("CIDADE: %s\n", cidade2);
+    printf("POPULAÇÃO: %d\n", populacao2);
+    printf("ÁREA: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos2);
+    printf("DENSIDADE POPULACIONAL: %.2f\n", den_populacional2);
+    printf("PIB PER CAPITA: %.2f\n", pib_pcapita2);
+    printf("SUPER PODER: %.2f\n", super_poder2);
+
+    // === COMPARAÇÕES ===
+    printf("\n=== RESULTADOS ===\n");
+
+    printf("\n=== POPULAÇÃO ===\n");
+    printf("CARTA #1 -> %d / CARTA #2 -> %d\n", populacao1, populacao2);
+    if (populacao1 == populacao2){
+        printf("EMPATE!\n");
+    }
+    else if (populacao1 > populacao2){
+        printf("%s VENCEU!\n", cidade1);
+    }
+    else{
+        printf("%s VENCEU!\n", cidade2);
+    }
+
+    return 0;
+}
