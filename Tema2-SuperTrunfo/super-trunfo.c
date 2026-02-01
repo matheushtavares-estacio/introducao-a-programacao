16a0fa9d25dc9c299be068f662af9dd224bca2ff
matheushtavares-estacio <202511091108@alunos.estacio.br>
2026-02-01 13:41:06 +0000

    Atividade nível Mestre: - Criação da variável Super Poder; - Implementação da aba de comparações.

--- Tema2-SuperTrunfo/super-trunfo.c
+++ Tema2-SuperTrunfo/super-trunfo.c
@@ -5,12 +5,14 @@ int main(){
     char estado1, estado2;
     char codigo1[10], codigo2[10];
     char cidade1[30], cidade2[30];
-    int populacao1, populacao2;
+    unsigned long int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int num_pturisticos1, num_pturisticos2;
     float den_populacional1, den_populacional2;
     float pib_pcapita1, pib_pcapita2;
+    float super_poder1, super_poder2;
+    float inv_den_populacional1, inv_den_populacional2;
 
     // === CADASTRO CARTA #1 ===
     printf("\n=== CADASTRO CARTA #1 ===\n");
@@ -25,7 +27,7 @@ int main(){
     scanf("%s", cidade1);
 
     printf("POPULAÇÃO: ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("ÁREA: ");
     scanf("%f", &area1);
@@ -49,7 +51,7 @@ int main(){
     scanf("%s", cidade2);
 
     printf("POPULAÇÃO: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("ÁREA: ");
     scanf("%f", &area2);
@@ -61,34 +63,73 @@ int main(){
     scanf("%d", &num_pturisticos2);
 
     // === CALCULOS ===
-    den_populacional1 = populacao1 / area1;
-    den_populacional2 = populacao2 / area2;
-    pib_pcapita1 = pib1 / populacao1;
-    pib_pcapita2 = pib2 / populacao2;   
+    den_populacional1 = (float)populacao1 / area1;
+    den_populacional2 = (float)populacao2 / area2;
+    pib_pcapita1 = pib1 / (float)populacao1;
+    pib_pcapita2 = pib2 / (float)populacao2;
+    inv_den_populacional1 = 1.0 / den_populacional1;
+    inv_den_populacional2 = 1.0 / den_populacional2;
+    super_poder1 = (float)populacao1 + area1 + pib1 + (float)num_pturisticos1 + pib_pcapita1 + inv_den_populacional1;
+    super_poder2 = (float)populacao2 + area2 + pib2 + (float)num_pturisticos2 + pib_pcapita2 + inv_den_populacional2;
 
     // === PRINT CARTA #1 ===
     printf("\n=== CARTA #1 ===\n");
     printf("ESTADO: %c\n", estado1);
     printf("CÓDIGO: %s\n", codigo1);
     printf("CIDADE: %s\n", cidade1);
-    printf("POPULAÇÃO: %d\n", populacao1);
+    printf("POPULAÇÃO: %lu\n", populacao1);
     printf("ÁREA: %.2f\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos1);
     printf("DENSIDADE POPULACIONAL: %.2f\n", den_populacional1);
     printf("PIB PER CAPITA: %.2f\n", pib_pcapita1);
+    printf("SUPER PODER: %.2f\n", super_poder1);
 
     // === PRINT CARTA #2 ===
     printf("\n=== CARTA #2 ===\n");
     printf("ESTADO: %c\n", estado2);
     printf("CÓDIGO: %s\n", codigo2);
     printf("CIDADE: %s\n", cidade2);
-    printf("POPULAÇÃO: %d\n", populacao2);
+    printf("POPULAÇÃO: %lu\n", populacao2);
     printf("ÁREA: %.2f\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos2);
     printf("DENSIDADE POPULACIONAL: %.2f\n", den_populacional2);
     printf("PIB PER CAPITA: %.2f\n", pib_pcapita2);
+    printf("SUPER PODER: %.2f\n", super_poder2);
+
+    // === COMPARAÇÕES ===
+    printf("\n=== RESULTADOS ===\n");
+    printf("1 -> CARTA #1 GANHOU!\n");
+    printf("0 -> CARTA #2 GANHOU!\n");
+
+    printf("\n=== POPULAÇÃO ===\n");
+    printf("CARTA #1 -> %lu / CARTA #2 -> %lu\n", populacao1, populacao2);
+    printf("RESULTADO: %d\n", populacao1 > populacao2);
+
+    printf("\n=== ÁREA ===\n");
+    printf("CARTA #1 -> %.2f / CARTA #2 -> %.2f\n", area1, area2);
+    printf("RESULTADO: %d\n", area1 > area2);
+
+    printf("\n=== PIB ===\n");
+    printf("CARTA #1 -> %.2f / CARTA #2 -> %.2f\n", pib1, pib2);
+    printf("RESULTADO: %d\n", pib1 > pib2);
+
+    printf("\n=== PONTOS TURÍSTICOS ===\n");
+    printf("CARTA #1 -> %d / CARTA #2 -> %d\n", num_pturisticos1, num_pturisticos2);
+    printf("RESULTADO: %d\n", num_pturisticos1 > num_pturisticos2);
+
+    printf("\n=== PIB PER CAPITA ===\n");
+    printf("CARTA #1 -> %.2f / CARTA #2 -> %.2f\n", pib_pcapita1, pib_pcapita2);
+    printf("RESULTADO: %d\n", pib_pcapita1 > pib_pcapita2);
+
+    printf("\n=== DENSIDADE POPULACIONAL ===\n");
+    printf("CARTA #1 -> %.2f / CARTA #2 -> %.2f\n", den_populacional1, den_populacional2);
+    printf("RESULTADO: %d\n", den_populacional1 < den_populacional2);
+
+    printf("\n=== SUPER PODER ===\n");
+    printf("CARTA #1 -> %.2f / CARTA #2 -> %.2f\n", super_poder1, super_poder2);
+    printf("RESULTADO: %d\n", super_poder1 > super_poder2);
 
     return 0;
 }
