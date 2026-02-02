e228fa0ee453ae8b86d93e95cbcef16caa89cf93
matheushtavares-estacio <202511091108@alunos.estacio.br>
2026-02-02 00:15:49 +0000

    Tema 3: Atividade Nível Aventureiro - Implementação do menu interativo;

--- Tema3-SuperTrunfo/super-trunfo.c
+++ Tema3-SuperTrunfo/super-trunfo.c
@@ -2,29 +2,19 @@
 
 int main(){
     // === VARIÁVEIS ===
-    char estado1, estado2;
-    char codigo1[10], codigo2[10];
-    char cidade1[30], cidade2[30];
+    char pais1[30], pais2[30];
     int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int num_pturisticos1, num_pturisticos2;
     float den_populacional1, den_populacional2;
-    float pib_pcapita1, pib_pcapita2;
-    float super_poder1, super_poder2;
-    float inv_den_populacional1, inv_den_populacional2;
+    int opcao;
 
     // === CADASTRO CARTA #1 ===
     printf("\n=== CADASTRO CARTA #1 ===\n");
-    
-    printf("ESTADO: ");
-    scanf(" %c", &estado1);
 
-    printf("CÓDIGO: ");
-    scanf("%s", codigo1);
-
-    printf("CIDADE: ");
-    scanf("%s", cidade1);
+    printf("PAÍS: ");
+    scanf("%s", pais1);
 
     printf("POPULAÇÃO: ");
     scanf("%d", &populacao1);
@@ -40,15 +30,9 @@ int main(){
 
     // === CADASTRO CARTA #2 ===
     printf("\n=== CADASTRO CARTA #2 ===\n");
-    
-    printf("ESTADO: ");
-    scanf(" %c", &estado2);
 
-    printf("CÓDIGO: ");
-    scanf("%s", codigo2);
-
-    printf("CIDADE: ");
-    scanf("%s", cidade2);
+    printf("PAÍS: ");
+    scanf("%s", pais2);
 
     printf("POPULAÇÃO: ");
     scanf("%d", &populacao2);
@@ -65,53 +49,100 @@ int main(){
     // === CALCULOS ===
     den_populacional1 = (float)populacao1 / area1;
     den_populacional2 = (float)populacao2 / area2;
-    pib_pcapita1 = pib1 / (float)populacao1;
-    pib_pcapita2 = pib2 / (float)populacao2;
-    inv_den_populacional1 = 1.0 / den_populacional1;
-    inv_den_populacional2 = 1.0 / den_populacional2;
-    super_poder1 = (float)populacao1 + area1 + pib1 + (float)num_pturisticos1 + pib_pcapita1 + inv_den_populacional1;
-    super_poder2 = (float)populacao2 + area2 + pib2 + (float)num_pturisticos2 + pib_pcapita2 + inv_den_populacional2;
 
-    // === PRINT CARTA #1 ===
-    printf("\n=== CARTA #1 ===\n");
-    printf("ESTADO: %c\n", estado1);
-    printf("CÓDIGO: %s\n", codigo1);
-    printf("CIDADE: %s\n", cidade1);
-    printf("POPULAÇÃO: %d\n", populacao1);
-    printf("ÁREA: %.2f\n", area1);
-    printf("PIB: %.2f\n", pib1);
-    printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos1);
-    printf("DENSIDADE POPULACIONAL: %.2f\n", den_populacional1);
-    printf("PIB PER CAPITA: %.2f\n", pib_pcapita1);
-    printf("SUPER PODER: %.2f\n", super_poder1);
-
-    // === PRINT CARTA #2 ===
-    printf("\n=== CARTA #2 ===\n");
-    printf("ESTADO: %c\n", estado2);
-    printf("CÓDIGO: %s\n", codigo2);
-    printf("CIDADE: %s\n", cidade2);
-    printf("POPULAÇÃO: %d\n", populacao2);
-    printf("ÁREA: %.2f\n", area2);
-    printf("PIB: %.2f\n", pib2);
-    printf("PONTOS TURÍSTICOS: %d\n", num_pturisticos2);
-    printf("DENSIDADE POPULACIONAL: %.2f\n", den_populacional2);
-    printf("PIB PER CAPITA: %.2f\n", pib_pcapita2);
-    printf("SUPER PODER: %.2f\n", super_poder2);
+    // === MENU INTERATIVO ===
+    printf("\n=== RESULTADOS ===\n");
+    printf("QUAIS ATRIBUTOS VOCÊ DESEJA COMPARAR?\n");
+    printf("[ 1 ] PAÍS;\n");
+    printf("[ 2 ] POPULAÇÃO;\n");
+    printf("[ 3 ] ÁREA;\n");
+    printf("[ 4 ] PIB;\n");
+    printf("[ 5 ] NÚMERO DE PONTOS TURÍSTICOS;\n");
+    printf("[ 6 ] DENSIDADE DEMOGRÁFICA;\n");
+    scanf("%d", &opcao);
 
     // === COMPARAÇÕES ===
-    printf("\n=== RESULTADOS ===\n");
+    switch (opcao)
+    {
+    case 1:
+        printf("OS PAÍSES INSERIDOS SÃO: %s E %s.\n", pais1, pais2);
+        break;
+    
+    case 2:
+        printf("%s => POPULAÇÃO => %d\n", pais1, populacao1);
+        printf("%s => POPULAÇÃO => %d\n", pais2, populacao2);
+        if (populacao1 > populacao2){
+            printf("%s GANHOU!\n", pais1);
+        }
+        else if (populacao2 > populacao1){
+            printf("%s GANHOU!\n", pais2);
+        }
+        else{
+            printf("EMPATE!\n");
+        }
+        break;
 
-    printf("\n=== POPULAÇÃO ===\n");
-    printf("CARTA #1 -> %d / CARTA #2 -> %d\n", populacao1, populacao2);
-    if (populacao1 == populacao2){
-        printf("EMPATE!\n");
-    }
-    else if (populacao1 > populacao2){
-        printf("%s VENCEU!\n", cidade1);
-    }
-    else{
-        printf("%s VENCEU!\n", cidade2);
+    case 3:
+        printf("%s => ÁREA => %f\n", pais1, area1);
+        printf("%s => ÁREA => %f\n", pais2, area2);
+        if (area1> area2){
+            printf("%s GANHOU!\n", pais1);
+        }
+        else if (area2 > area1){
+            printf("%s GANHOU!\n", pais2);
+        }
+        else{
+            printf("EMPATE!\n");
+        }
+        break;
+
+    case 4:
+        printf("%s => PIB => %f\n", pais1, pib1);
+        printf("%s => PIB => %f\n", pais2, pib2);
+        if (pib1 > pib2){
+            printf("%s GANHOU!\n", pais1);
+        }
+        else if (pib2 > pib1){
+            printf("%s GANHOU!\n", pais2);
+        }
+        else{
+            printf("EMPATE!\n");
+        }
+        break;
+
+    case 5:
+        printf("%s => NÚMERO DE PONTOS TURÍSTICOS => %d\n", pais1, num_pturisticos1);
+        printf("%s => NÚMERO DE PONTOS TURÍSTICOS => %d\n", pais2, num_pturisticos2);
+        if (num_pturisticos1 > num_pturisticos2){
+            printf("%s GANHOU!\n", pais1);
+        }
+        else if (num_pturisticos2 > num_pturisticos1){
+            printf("%s GANHOU!\n", pais2);
+        }
+        else{
+            printf("EMPATE!\n");
+        }
+        break;
+
+    case 6:
+        printf("%s => DENSIDADE DEMOGRÁFICA => %f\n", pais1, den_populacional1);
+        printf("%s => DENSIDADE DEMOGRÁFICA => %f\n", pais2, den_populacional2);
+        if (den_populacional1 < den_populacional2){
+            printf("%s GANHOU!\n", pais1);
+        }
+        else if (den_populacional2 < den_populacional1){
+            printf("%s GANHOU!\n", pais2);
+        }
+        else{
+            printf("EMPATE!\n");
+        }
+        break;
+
+    default:
+        printf("OPÇÃO INVÁLIDA!\n");
+        break;
     }
 
+    printf("PROGRAMA ENCERRADO!");
     return 0;
 }
