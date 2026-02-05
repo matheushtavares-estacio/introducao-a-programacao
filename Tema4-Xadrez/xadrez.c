77b5c70febdcdaa7cb3c8bcab49d53b594801fb7
matheushtavares-estacio <202511091108@alunos.estacio.br>
2026-02-05 17:41:31 +0000

    Tema 4: Nível Aventureiro - Implementação do movimento do Cavalo através de loops aninhados; - Desafio extra com movimento do Cavalo com menu interativo.

--- Tema4-Xadrez/xadrez.c
+++ Tema4-Xadrez/xadrez.c
@@ -2,14 +2,16 @@
 
 int main(){
     // === CONTADORES ===
-    int cont1 = 1, cont2 = 1, cont3 = 1;
+    int iTorre = 1;
+    int iBispo = 1; 
+    int continuar = 1, quantidadeCasasH, quantidadeCasasV, opcaoVertical, opcaoHorizontal;
 
     // === TORRE (While) ===
     printf("\nJogador #1 está movendo a Torre...\n");
-    while (cont1 <= 5)
+    while (iTorre <= 5)
     {
         printf("DIREITA\n");
-        cont1++;
+        iTorre++;
     }
 
     // === TORRE ===
@@ -17,15 +19,73 @@ int main(){
     do
     {
         printf("DIAGONAL (DIREITA + CIMA)\n");
-        cont2++;
-    } while (cont2 <= 5);
+        iBispo++;
+    } while (iBispo <= 5);
  
     // === RAINHA ===
     printf("\nO Jogador #1 está movendo a Rainha...\n");
-    for (cont3; cont3 <= 8; cont3++)
+    for (int iRainha = 1; iRainha <= 8; iRainha++)
     {
         printf("ESQUERDA\n");
     }
+
+    // === CAVALO - VERSÃO 1.0 (Loops aninhados) ===
+    printf("\nO Jogador #1 está movendo o Cavalo...\n");
+    for (int iCavalo = 1; iCavalo <= 1 ; iCavalo++)
+    {
+        for (int jCavalo = 1; jCavalo <= 2; jCavalo++)
+        {
+            printf("BAIXO\n");
+        }
+        printf("ESQUERDA\n");
+    }
     
+    // === CAVALO - VERSÃO 2.0 (Menu interativo) ===
+    printf("\nO Jogador #1 está movendo o Cavalo...\n");
+
+    while (continuar != 0)
+    {
+        // === MENU INTERATIVO ===
+        printf("Defina o movimento HORIZONTAL:\n");
+        printf("[ 1 ] ESQUERDA\n");
+        printf("[ 2 ] DIREITA\n>> ");
+        scanf("%d", &opcaoHorizontal);
+
+        printf("Quantas casas irá se mover no sentido HORIZONTAL?\n");
+        printf("[ 1 ] 1 CASA\n");
+        printf("[ 2 ] 2 CASAS\n>> ");
+        scanf("%d", &quantidadeCasasH);
+
+        quantidadeCasasV = 3 - quantidadeCasasH;
+
+        printf("Defina o movimento VERTICAL:\n");
+        printf("[ 1 ] CIMA\n");
+        printf("[ 2 ] BAIXO\n>> ");
+        scanf("%d", &opcaoVertical);
+
+        if ((opcaoVertical == 1 || opcaoVertical == 2) && (opcaoHorizontal == 1 || opcaoHorizontal == 2) && (quantidadeCasasH == 1 || quantidadeCasasH == 2))
+        {
+            for (int j = 1; j <= quantidadeCasasH; j++)
+            {
+                opcaoHorizontal == 1? printf("ESQUERDA\n") : printf("DIREITA\n");
+            }
+            for (int i = 1; i <= quantidadeCasasV; i++)
+            {        
+                opcaoVertical == 1? printf("CIMA\n") : printf("BAIXO\n");
+            }
+        }
+        else{
+            printf("Entrada INVÁLIDA! Tente novamente...\n");
+        }
+
+        // === CONTINUAR ===
+        printf("Deseja continuar?\n");
+        printf("Digite qualquer número para continuar...\n");
+        printf("Digite '0' para parar...\n>> ");
+        scanf("%d", &continuar);
+    }
+    
+    printf("\nPrograma encerrado.\n");
+
     return 0;
 }
