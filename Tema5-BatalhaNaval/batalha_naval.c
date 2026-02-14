aeb2596ac16b7a7d7445dd6ad00cf78a37ed86a5
matheushtavares-estacio <202511091108@alunos.estacio.br>
2026-02-14 15:12:11 +0000

    Desafio Nível Aventureiro: - Implementação de dois novos navios na diagonal.

--- Tema5-BatalhaNaval/batalha_naval.c
+++ Tema5-BatalhaNaval/batalha_naval.c
@@ -3,9 +3,13 @@
 // VARIÁVEIS
 int tabuleiro[10][10] = {0};
 int navioHorizontal[3] = {3, 3, 3}; 
-int navioVertical[4] = {3, 3, 3, 3};
-int linhaNavioH = 2, colunaNavioH = 1;
-int linhaNavioV = 1, colunaNavioV = 6;
+int navioVertical[3] = {3, 3, 3};
+int navioDiagonal1[3] = {3, 3, 3};
+int navioDiagonal2[3] = {3, 3, 3};
+int linhaNavioH = 0, colunaNavioH = 0;
+int linhaNavioV = 2, colunaNavioV = 2;
+int linhaNavioD1 = 2, colunaNavioD1 = 4;
+int linhaNavioD2 = 6, colunaNavioD2 = 8;
 
 int main(){
     // POSICIONAR NAVIO HORIZONTAL
@@ -16,10 +20,26 @@ int main(){
     }
 
     // POSICIONAR NAVIO VERTICAL
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
     {
         tabuleiro[linhaNavioV][colunaNavioV] = navioVertical[i];
         linhaNavioV++;
+    }
+
+    // POSICIONAR SEGUNDO NAVIO DIAGONAL
+    for (int i = 0; i < 3; i++)
+    {
+        tabuleiro[linhaNavioD2][colunaNavioD2] = navioDiagonal2[i];
+        linhaNavioD2++;
+        colunaNavioD2--;
+    }
+
+    // POSICIONAR PRIMEIRO NAVIO DIAGONAL
+    for (int i = 0; i < 3; i++)
+    {
+        tabuleiro[linhaNavioD1][colunaNavioD1] = navioDiagonal1[i];
+        linhaNavioD1++;
+        colunaNavioD1++;
     }
 
     // PRINT TABULEIRO
